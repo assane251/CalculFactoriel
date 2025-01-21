4f7073d10548a03d4c6b4782cbb20c18bb1600da
Al Hassane DIALLO <assanedialloisidk@groupeisi.com>
2025-01-21 02:11:17 +0000

    erreur de Main.java

--- src/main/java/gm/rahmanproperties/Main.java
+++ src/main/java/gm/rahmanproperties/Main.java
@@ -3,6 +3,7 @@ package gm.rahmanproperties;
 public class Main {
     public static void main(String[] args) {
         int calculateFactoriel = CalculFactoriel.calculateFactoriel(5);
+        salut
         System.out.println(calculateFactoriel);
     }
 }
